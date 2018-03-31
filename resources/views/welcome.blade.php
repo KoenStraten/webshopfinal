f10f9dace1c1b7b277a7396dfa01a44a6283301a
koenstraten <kstraten@avans.nl>
2018-03-31 15:54:41 +0200

    matje

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,8 @@
                 <div class="container">
                     <div class="carousel-caption text-left carousel-background">
                         <h1>Benieuwd naar ons bedrijf?.</h1>
-                        <p>Wij willen natuurlijk de lekkerste en de meest biologische kaas, om dit te bereiken geven wij onze koeien een prachtig leventje, kijk dan toch hoe blij hij is.</p>
+                        <p>Wij willen natuurlijk de lekkerste en de meest biologische kaas, om dit te bereiken geven wij
+                            onze koeien een prachtig leventje, kijk dan toch hoe blij hij is.</p>
                         <p><a class="btn btn-lg btn-primary" href="/../about" role="button">Meer weten?</a></p>
                     </div>
                 </div>
@@ -24,8 +25,10 @@
                 <div class="container">
                     <div class="carousel-caption carousel-background">
                         <h1>Lekker veel korting!</h1>
-                        <p>Omdat de lucht natuurlijk zo prachtig en mooi blauw is op deze foto, kun je met de volgende actie code: "MooieBlauweLucht23,6" nu 25% korting krijgen op je allereerste aankoop!</p>
-                        <p><a class="btn btn-lg btn-primary" href="/../categoryoverview" role="button">Bekijk onze producten!</a></p>
+                        <p>Omdat de lucht natuurlijk zo prachtig en mooi blauw is op deze foto, kun je met de volgende
+                            actie code: "MooieBlauweLucht23,6" nu 25% korting krijgen op je allereerste aankoop!</p>
+                        <p><a class="btn btn-lg btn-primary" href="/../categoryoverview" role="button">Bekijk onze
+                                producten!</a></p>
                     </div>
                 </div>
             </div>
@@ -34,8 +37,10 @@
                 <div class="container">
                     <div class="carousel-caption text-right carousel-background">
                         <h1>Meest verkochte product!</h1>
-                        <p>De kaas hierachter ziet er toch heerlijk uit, vind je ook niet? Koop daarom nu het meest verkochte product!</p>
-                        <p><a class="btn btn-lg btn-primary" href="/../product/{{ $populairProduct->id }}" role="button">Bekijk product</a></p>
+                        <p>De kaas hierachter ziet er toch heerlijk uit, vind je ook niet? Koop daarom nu het meest
+                            verkochte product!</p>
+                        <p><a class="btn btn-lg btn-primary" href="/../product/{{ $populairProduct->id }}"
+                              role="button">Bekijk product</a></p>
                     </div>
                 </div>
             </div>
@@ -83,17 +88,17 @@
                                     <p class="price">{{ "$" . $p->price }}</p>
                                     <a href="../product/{{ $p->id }}" class="btn btn-warning">To product page ></a>
                                     {{--@if(\Illuminate\Support\Facades\Auth::check())--}}
-                                        <form class="pt-3" method="POST" action="../shoppingcart/store/">
-                                            {{ csrf_field() }}
-                                            <input type="hidden" name="product" value="{{ $p->id }}">
-                                            <button type="submit" class="btn btn-block btn-warning"><i
-                                                        class="fas fa-plus"></i>
-                                                In winkelwagen
-                                            </button>
-                                        </form>
-                                    {{--@else--}}
-                                    {{--<button disabled class="mt-3 btn btn-block btn-warning">Log in</button>--}}
-                                    {{--@endif--}}
+                                    <form class="pt-3" method="POST" action="../shoppingcart/store/">
+                                        {{ csrf_field() }}
+                                        {{--<input type="hidden" name="product" value="{{ $p->id }}">--}}
+                                        {{--<button type="submit" class="btn btn-block btn-warning"><i--}}
+                                        {{--class="fas fa-plus"></i>--}}
+                                        {{--In winkelwagen--}}
+                                        {{--</button>--}}
+                                        <input type="hidden" name="product" value="{{ $product->id }}">
+                                        <input class="btn btn-block btn-primary" type="submit"><i
+                                                class="fas fa-plus"></i>In winkelwagen
+                                    </form>
                                 </div>
                             </div>
                             @endforeach
