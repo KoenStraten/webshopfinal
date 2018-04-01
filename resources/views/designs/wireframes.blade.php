970cca9c0af3e807cafadfa5aa1626e7323bc58d
koenstraten <kstraten@avans.nl>
2018-04-01 13:48:46 +0200

    wireframes

--- resources/views/designs/wireframes.blade.php
+++ resources/views/designs/wireframes.blade.php
@@ -2,24 +2,41 @@
 @section ('content')
     {{--    {{ Breadcrumbs::render('wireframes') }}--}}
 
-    <div class="container">
+    <div class="container text-center">
         <h3 class="pt-3">Wireframes</h3>
-        <div class="my-3 p-3 bg-white rounded box-shadow">
-            <div class="row">
-                <h4>Home</h4>
-                <img class="w-100" src="/images/wireframes/home.jpg">
-            </div>
-            <div class="row">
-                <img class="w-100" src="/images/wireframes/about.png">
-            </div>
-            <div class="row">
-                <img class="w-100" src="/images/wireframes/product.png">
-            </div>
-            <div class="row">
-                <img class="w-100" src="/images/wireframes/shoppingcart.png">
-            </div>
-            <div class="row">
-                <img class="w-100" src="/images/wireframes/categorieen.jpg">
-            </div>
+        <div class="my-3 p-3 bg-white rounded box-shadow justify-content-center">
+            <h4 class="pt-3">Home ingelogd</h4>
+            <img class="w-100" src="/images/wireframes/home_ingelogd.png">
+
+            <h4 class="pt-3">Home uitgelogd</h4>
+            <img class="w-100" src="/images/wireframes/home_uitgelogd.png">
+
+            <h4 class="pt-3">Inloggen</h4>
+            <img class="w-100" src="/images/wireframes/login.png">
+
+            <h4 class="pt-3">Betaling</h4>
+            <img class="w-100" src="/images/wireframes/payment.png">
+
+            <h4 class="pt-3">Product</h4>
+            <img class="w-100" src="/images/wireframes/product.png">
+
+            <h4 class="pt-3">Aankoopgeschiedenis</h4>
+            <img class="w-100" src="/images/wireframes/purchasehistory.png">
+
+            <h4 class="pt-3">Registreren</h4>
+            <img class="w-100" src="/images/wireframes/register.png">
+
+            <h4 class="pt-3">Winkelwagen</h4>
+            <img class="w-100" src="/images/wireframes/shoppingcart.png">
+
+            <h4 class="pt-3">About</h4>
+            <img class="w-100" src="/images/wireframes/about.png">
+
+            <h4 class="pt-3">Shopping cart</h4>
+            <img class="w-100" src="/images/wireframes/shoppingcart.png">
+
+            <h4 class="pt-3">Categorieën</h4>
+            <img class="w-100" src="/images/wireframes/categorieen.jpg">
         </div>
+    </div>
 @endsection
